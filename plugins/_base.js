7d8c34078a91bb4713b3629380d600ecf462b473
Tseng <tseng7510@gmail.com>
2024-11-11 19:21:42 +0800

    更新

--- plugins/_base.js
+++ plugins/_base.js
@@ -64,24 +64,25 @@ window.addEventListener('scroll', function () {
 // cookie
 
 //  GOTOP
-// (function () {
-//   let goTopBtn = document.querySelector('.goTop');
+(function () {
+  let goTopBtn = document.querySelector('.goTop');
 
-//   window.addEventListener('scroll', function () {
-//     let windowScrollTop = document.documentElement.scrollTop;
-//     if (windowScrollTop >= 200) {
-//       goTopBtn.style.cssText = 'opacity:1';
-//     } else {
-//       goTopBtn.style.cssText = 'opacity:0';
-//     }
-//   });
-//   goTopBtn.addEventListener('click', function () {
-//     window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth',
-//     });
-//   });
-// })();
+  // window.addEventListener('scroll', function () {
+  //   let windowScrollTop = document.documentElement.scrollTop;
+  //   if (windowScrollTop >= 200) {
+  //     goTopBtn.style.cssText = 'opacity:1';
+  //   } else {
+  //     goTopBtn.style.cssText = 'opacity:0';
+  //   }
+  // });
+  goTopBtn.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+})();
+
 // head搜尋
 (function () {
   $('.menuSearch > button').on('click', function () {
